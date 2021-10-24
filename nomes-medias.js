83bafb7603a5cd73de63e9719d57806940190a1a
Renato Almeida <almeida.renato1988@gmail.com>
2021-10-24 14:54:44 -0300

    Exercícios da aula 2 do módulo 2

---
+++ nomes-medias.js
@@ -0,0 +1,13 @@
+const nomes = ["João", "Juliana", "Caio", "Ana"]
+
+const medias = [10, 8, 7.5, 9]
+
+let listaDeNotasEAlunos = [nomes, medias]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é: ${listaDeNotasEAlunos[1][0]}`)
+
+console.log(`${listaDeNotasEAlunos[0][1]}, sua média é: ${listaDeNotasEAlunos[1][1]}`)
+
+console.log(`${listaDeNotasEAlunos[0][2]}, sua média é: ${listaDeNotasEAlunos[1][2]}`)
+
+console.log(`${listaDeNotasEAlunos[0][3]}, sua média é: ${listaDeNotasEAlunos[1][3]}`)
